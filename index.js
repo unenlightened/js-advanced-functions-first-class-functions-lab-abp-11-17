9d973a18ac7d6652f1fb72b7d28bbadd07954342
Chely Ho <adept_inept@yahoo.com>
2017-11-07 18:20:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const returnLastTwoDrivers = function (array){
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 function createFareMultiplier(multiplier){
-  
-  return (function(fare, multiplier){ fare * multiplier;});
+
+  return (function(fare){ fare * multiplier;});
 }
+
+const fareMultiplier = createFareMultiplier(2);
+
+const fareQuintupler = createFareMultiplier(5);
+
+expect(fareQuintupler(5)).to.eql(25);
