136117d38afab0ef6bda3aa921ca13aae8b0d48b
Chely Ho <adept_inept@yahoo.com>
2017-11-07 17:19:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function (collection){
-  for (const key in collection){
+  for ((const key in collection) >){
     return console.log(collection);
   }
 };
