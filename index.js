13fb8bfc5d63823e5f8289c9848e6a7fc2618143
Chely Ho <adept_inept@yahoo.com>
2017-11-07 18:03:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,40 @@ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 function createFareMultiplier(fareMultiplier){
 
+  return (function (){});
+
 }
+
+/*describe('createFareMultiplier()', function () {
+  it('returns a function', function () {
+    const fareMultiplier = createFareMultiplier(2);
+
+    expect(fareMultiplier).to.be.a('function');
+  });
+
+  it('should multiply a given value using the created multiplier', function () {
+    const fareQuintupler = createFareMultiplier(5);
+
+    expect(fareQuintupler(5)).to.eql(25);
+  });
+});
+
+describe('fareDoubler()', function () {
+  it('is a function', function () {
+    expect(fareDoubler).to.be.a('function');
+  });
+
+  it('doubles fares', function () {
+    expect(fareDoubler(10)).to.eql(20);
+  });
+});
+
+describe('fareTripler()', function () {
+  it('is a function', function () {
+    expect(fareTripler).to.be.a('function');
+  });
+
+  it('triples fares', function() {
+    expect(fareTripler(12)).to.eql(36);
+  });
+}); */
