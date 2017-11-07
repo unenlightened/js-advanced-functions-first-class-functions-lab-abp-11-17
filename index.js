051090b8d446368be3aebd39ee8d20fb1af526d2
Chely Ho <adept_inept@yahoo.com>
2017-11-07 17:49:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ const returnLastTwoDrivers = function (array){
   const newArray = array.slice(array.length-2, array.length);
   return newArray;
 };
+
+function selectingDrivers(){
+  
+}
+/* describe('selectingDrivers', function () {
+  it('has the `returnFirstTwoDrivers` function to as its first element', function () {
+    expect(selectingDrivers[0]).to.eql(returnFirstTwoDrivers);
+  });
+
+  it('has the `returnLastTwoDrivers` function to as its last element', function () {
+    expect(selectingDrivers[1]).to.eql(returnLastTwoDrivers);
+  });
+
+  it('allows us to invoke either function from the array', function () {
+    expect(selectingDrivers[0](drivers)).to.eql(['Sally', 'Bob']);
+
+    expect(selectingDrivers[1](drivers)).to.eql(['Freddy', 'Claudia']);
+  });
+});
+*/
