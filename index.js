8083ac6b5ff700489fb2490075260fdeb5ba7229
Chely Ho <adept_inept@yahoo.com>
2017-11-07 17:41:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function (array){
+  const newArray = [];
+  
   for (i = 0; i < 2; i++){
-    console.log(array[i]);
+    newArray.push(array[i]);
   }
 
   return newArray;
