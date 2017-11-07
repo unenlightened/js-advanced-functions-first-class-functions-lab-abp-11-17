3130297ed72f1727267ccbdf3b7b8ceec396d929
Chely Ho <adept_inept@yahoo.com>
2017-11-07 17:31:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function (array){
-  for (const data in array){
-    if (data < 2){
+  for (i = 0; i++; i<2){
       return console.log(array);
     }
   }
